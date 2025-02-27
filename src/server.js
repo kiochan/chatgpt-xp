d501048071d6e967cf97c985d632c141f625dcd1
kiochan <kio@kiochan.one>
2025-02-27 09:49:57 +0100

    use old version showdown waht without emojis

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const TOKEN_LENGTH = 16;
 
 const FILES_TO_DOWNLOAD = {
   "showdown.min.js":
-    "http://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/showdown/1.7.6/showdown.min.js",
 };
 
 if (!fs.existsSync(DOWNLOAD_DIR)) {
